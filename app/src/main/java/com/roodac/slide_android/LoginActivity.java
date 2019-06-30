43267bb24a798ad84bfe801a7d854e3a72f70afb
Samuel Lee <samuel.r.lee@vanderbilt.edu>
2019-06-29 20:27:47 -0400

    Login function
    Integrated with firebase authentication. Facebook, Google and registration still not up.

--- app/src/main/java/com/roodac/slide_android/LoginActivity.java
+++ app/src/main/java/com/roodac/slide_android/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.roodac.slide_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +21,7 @@ import java.util.Map;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String LOG_TAG  = LoginActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
     EditText emailEditText;
     EditText passwordEditText;
 
@@ -22,20 +30,38 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         emailEditText    = findViewById(R.id.emailField);
         passwordEditText = findViewById(R.id.passwordField);
     }
 
+    // Handle when Login button is clicked.
     public void login(View view) {
+
         Map<String, EditText> signInInfo = new HashMap<>();
         signInInfo.put("email", emailEditText);
         signInInfo.put("password", passwordEditText);
 
+        // Verify that user information is correct
         if (TextFieldParser.validate(signInInfo)) {
-            Log.d("Log", "HELLOOOOOOO");
-            Intent intent = new Intent(this, HomeActivity.class);
-            startActivity(intent);
-            Log.d(LOG_TAG, "Logged in successfully!");
+            mAuth.signInWithEmailAndPassword(emailEditText.getText().toString(),
+                    passwordEditText.getText().toString()).addOnCompleteListener(this,
+                        new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                        Log.d(LOG_TAG, "Logged in successfully!");
+                    }
+                    else {
+                        Log.w(LOG_TAG, "Failed to log in", task.getException());
+                        // TODO: Toast is probably not the best error notification method as user can easily miss it.
+                        Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
         }
     }
 }
